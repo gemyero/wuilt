78c6eac860ba00adcd117c74e11ab9dcec905295
Mohamed Gamal <mohamed.rady@fixedmea.com>
2018-12-07 02:28:56 +0200

    remove token

--- src/utils/api/users-api.js
+++ src/utils/api/users-api.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 export const fetchUsers = () => {
-  return axios.get('https://api.github.com/users?per_page=10&access_token=933b27ffe08e9154b82c3e74dabc0fd9183ce4d7').then(result => result.data);
+  return axios.get('https://api.github.com/users?per_page=10').then(result => result.data);
 };
 
 export const fetchMoreUsers = (number, since) => {
-  return axios.get(`https://api.github.com/users?per_page=${number}&since=${since}&access_token=933b27ffe08e9154b82c3e74dabc0fd9183ce4d7`).then(result => result.data);
+  return axios.get(`https://api.github.com/users?per_page=${number}&since=${since}`).then(result => result.data);
 }
 
 export const fetchOneUser = (login) => {
-  return axios.get(`https://api.github.com/users/${login}?access_token=933b27ffe08e9154b82c3e74dabc0fd9183ce4d7`).then(result => result.data);
+  return axios.get(`https://api.github.com/users/${login}`).then(result => result.data);
 }
